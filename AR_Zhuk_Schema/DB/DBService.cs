605e68587a51641b91de572b2eb35db4dcbfe98a
Vildar Khisyametdinov <vildar82@gmail.com>
2016-08-19 13:33:27 +0300

    minArea +5-5

--- AR_Zhuk_Schema/DB/DBService.cs
+++ AR_Zhuk_Schema/DB/DBService.cs
@@ -90,7 +90,7 @@ namespace AR_Zhuk_Schema.DB
 
                             foreach (var r in sp.requirments.Where(x => x.CodeZone.Equals(f.SubZone)).ToList())
                             {
-                                if (!(r.MinArea-5<= f.AreaTotalStandart & r.MaxArea+5 >= f.AreaTotalStandart))
+                                if (!(r.MinArea<= f.AreaTotalStandart & r.MaxArea >= f.AreaTotalStandart))
                                     continue;
                                 isContains = true;
                                 break;
